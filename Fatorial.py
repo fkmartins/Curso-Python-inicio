d70af39c27b3c0bcb5c5a04e42aa439dd824cd8b
Frank Martins <frankmartins@gmail.com>
2018-10-29 00:06:03 -0400

    Update Fatorial.py

---
+++ Fatorial.py
@@ -0,0 +1,28 @@
+
+numero = int(input("Entre com um Número:"))
+
+c=0
+c1=1
+n=0
+n1=0
+
+
+while c < numero :
+    c = c + 1
+    c1 = c1 + 1
+    n1 = (c*c1)
+    
+    print("O numero é ", c, c1, n1)
+
+    
+    n1= (n*n1)
+
+    
+
+    
+  
+  
+print("O numero é ", c, n, n1)
+
+    
+    
