b31ff63a0a1a4b3046ed55fe35eeecb7b0a50114
Frank Martins <frankmartins@gmail.com>
2018-10-29 00:06:27 -0400

    Update media.py.py

---
+++ media.py.py
@@ -0,0 +1,21 @@
+texto = ("digite o texto:")
+print (texto)
+print ("Exercício 2")
+
+print ("------------------------------------------")
+
+primeiranota = float(input("Digite a primeira nota: "))
+
+segundanota  = float(input("Digite a segunda nota : "))
+
+terceiranota = float(input("Digite a terceira nota: "))
+
+quartanota   = float(input("Digite a quarta nota  : "))
+
+media = (primeiranota + segundanota + terceiranota + quartanota)/4
+
+print()
+
+print("A média aritimética é",media)
+
+
