0517ccdbd1053bd067e007bdcc53518da38fae74
Frank Martins <frankmartins@gmail.com>
2018-10-29 00:06:07 -0400

    Update Crescente.py

---
+++ Crescente.py
@@ -0,0 +1,13 @@
+
+x = int(input("Entre com o primeiro Número inteiro:"))
+y = int(input("Entre com o segundo Número inteiro:"))
+z = int(input("Entre com o terceiro Número inteiro:"))
+
+if x <= y:
+    if y <= z :
+        print("crescente")
+    else:
+        print("não está em ordem crescente")
+else:
+    print("não está em ordem crescente")
+    
