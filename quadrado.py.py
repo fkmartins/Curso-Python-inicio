5d9993edb5e1af24bcb129b86b2cecf6d17d62b3
Frank Martins <frankmartins@gmail.com>
2018-10-29 00:05:33 -0400

    Update quadrado.py.py

---
+++ quadrado.py.py
@@ -0,0 +1,17 @@
+print ("Exercício 1")
+
+print ("-----------------------------------------------------------------------------")
+
+print()
+
+ladoquadrado = int(input("Digite o valor corresponddente ao lado de um quadrado: "))
+
+area = ladoquadrado ** 2
+
+perimetro = ladoquadrado * 4
+
+print()
+
+print("perímetro:",perimetro,"- área:",area)
+
+
