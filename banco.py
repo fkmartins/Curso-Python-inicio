8c43b5635f8f6e4c77119969138b6fffaaf843e1
Frank Martins <frankmartins@gmail.com>
2018-10-29 00:04:38 -0400

    Update banco.py

---
+++ banco.py
@@ -0,0 +1,17 @@
+print ("Exercício 1")
+
+print ("-----------------------------------------------------------------------------")
+
+print()
+
+nomecliente = input("Digite o nome do cliente: ")
+diavencimento = input("Digite o dia de vencimento: ")
+mesvencimento = input("Digite o mês de vencimento: ")
+valorfatura = input("Digite o valor da fatura: ")
+
+print()
+
+print("Olá,",nomecliente)
+print("A sua fatura com vencimento em",diavencimento,"de",mesvencimento,"no valor de R$",valorfatura,"está fechada.")
+
+
